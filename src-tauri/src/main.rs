8196aa56034c8bd1e4ffce6d23ced399886102e1
chami2000 <chami44@proton.me>
2024-11-11 23:18:23 +0530

    Create main.rs

---
+++ src-tauri/src/main.rs
@@ -0,0 +1,6 @@
+# In Cargo.toml
+[dependencies]
+tauri = { version = "1.0", features = ["api-all"] }
+serde = { version = "1.0", features = ["derive"] }
+serde_json = "1.0"
+reqwest = { version = "0.11", features = ["json"] }
