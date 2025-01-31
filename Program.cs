9b32ac25161f5f1598e45ae62079e75619b45396
chami2000 <chami44@proton.me>
2025-01-31 11:01:13 +0530

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -1,1 +1,16 @@
+using System;
+using System.Windows.Forms;
 
+namespace LTEBandControl
+{
+    static class Program
+    {
+        [STAThread]
+        static void Main()
+        {
+            Application.EnableVisualStyles();
+            Application.SetCompatibleTextRenderingDefault(false);
+            Application.Run(new MainForm());
+        }
+    }
+}
